2d2d5541f0796dc0e5072802ed5307ad9b97b8dc
YaChugunov <informed.looser@gmail.com>
2022-04-01 14:34:12 +0300

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './css/main.css';
 
-export default function App() {
+// Импортируем данные из json-файла и передаем в компонент.
+import jsonData from './data/etsy.json';
+
+function Listing(props) {
+  console.log(props.MainImage);
+  const MainImage = props.MainImage;
+  //const url = MainImage.map((elem) => (elem.url))
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div class="item">
+      <div class="item-image">
+        <a href={props.url}>
+          <img src={props.MainImage} />
+        </a>
+      </div>
+      <div class="item-details">
+        <p class="item-title">{props.title}</p>
+        <p class="item-price">
+          {props.currency_code}
+          {props.price}
+        </p>
+        <p class="item-quantity level-medium">{props.quantity}</p>
+      </div>
     </div>
   );
+}
+
+export default function App() {
+  // Формирование массива компонентов
+  const items = jsonData.map((item) => (
+    <Listing
+      key={item.listing_id}
+      listing_id={item.listing_id}
+      url={item.url}
+      MainImage={item.MainImage}
+      title={item.title}
+      currency_code={item.currency_code}
+      price={item.price}
+      quantity={item.quantity}
+    />
+  ));
+  return <div class="item-list">{items}</div>;
 }
